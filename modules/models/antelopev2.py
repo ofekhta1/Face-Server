5c775ec18d97ef5ea99b2339d6eb743a2afca6d6
DanielGrin31 <grinberg323@gmail.com>
2024-04-21 12:15:45 +0300

    Changed model loading to local
    Update gitignore

--- modules/models/antelopev2.py
+++ modules/models/antelopev2.py
@@ -2,7 +2,7 @@ import sys
 import os
 import traceback
 import insightface
-from insightface.app import FaceAnalysis
+from .face_analysis import FaceAnalysis
 from .base_model import BaseModel
 sys.path.append(os.path.abspath('..'))
 from ..util import are_bboxes_similar
